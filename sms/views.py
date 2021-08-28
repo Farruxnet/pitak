7bfe5f6bf0f1cbc7fe203c46af93dfa0fbc3d3af
Farruxnet <farruxbekinfo@gmail.com>
2021-08-28 16:25:46 +0500

    docs update

--- sms/views.py
+++ sms/views.py
@@ -4,10 +4,12 @@ from .serializer import SmsSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from . sms_service import sms_sender
+from drf_yasg.utils import swagger_auto_schema
 
 class SmsView(APIView):
     VALIDATE_CLASS = None
     permission_classes = [AllowAny]
+    @swagger_auto_schema(request_body=SmsSerializer)
     def post(self, request):
         serializer = SmsSerializer(data = request.data)
         serializer.is_valid(raise_exception=True)
