9ae9ae03cd936dc846f2a1abae6b8d92f4bdce0a
Farruxnet <farruxbekinfo@gmail.com>
2021-08-28 15:49:09 +0500

    create docs

---
+++ doc/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from .models import Snippet
+from .serializer import SnippetSerializer
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
