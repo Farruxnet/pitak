2e2115f096dc77db05e7733f72d7cb1147b077fd
Farruxnet <farruxbekinfo@gmail.com>
2021-08-28 16:25:28 +0500

    urls update

--- settings/settings.py
+++ settings/settings.py
@@ -68,6 +68,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',  # <-- And here
+        'rest_framework.authentication.SessionAuthentication',
     ],
 }
 
--- settings/urls.py
+++ settings/urls.py
@@ -6,26 +6,24 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 from sms.views import SmsView
-from rest_framework.documentation import include_docs_urls
 
 schema_view = get_schema_view(
     openapi.Info(
         title="PITAK24 API DOCUMENTATION",
         description="REST API",
         default_version="VERSION 1",
-        terms_of_service="https://google.com/poli",
+        terms_of_service="https://t.me/pitak24_bot",
         contact = openapi.Contact(email="farruxbekinfo@gmail.com"),
-        license=openapi.License(name="Public"),
+        license=openapi.License(name="Private"),
     ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
+    public=False,
+    permission_classes=(permissions.IsAdminUser,),
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('pitak-api-admin-farruxnet/', admin.site.urls),
     path('api/v1/', include('sms.urls')),
     path('api/v1/', include('users.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('docs/', include_docs_urls(title='Snippet API')),
+    path('docs-api-swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs-api-redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
